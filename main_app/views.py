b969d6e30a0341ed61176d78b0813b63ea15ba61
lancechincodes <lanceechin@gmail.com>
2022-08-21 16:17:21 -0500

    Add assets

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View # View class to handle requests
+from django.http import HttpResponse # a class to handle sending a type of response 
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+# extending from View class
+class Home(TemplateView):
+    template_name = "home.html"
