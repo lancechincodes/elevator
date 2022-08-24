11413c3e001c9b214369ada6ca0ad32e426b35be
lancechincodes <lanceechin@gmail.com>
2022-08-24 15:53:00 -0500

    Config deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn elevator_project.wsgi
--- elevator_project/settings.py
+++ elevator_project/settings.py
@@ -127,3 +127,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
