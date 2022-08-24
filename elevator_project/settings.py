0235d9ee9fd1bd4194fe2bb68746f8418136f858
lancechincodes <lanceechin@gmail.com>
2022-08-24 16:13:23 -0500

    Config deployment p2

--- elevator_project/settings.py
+++ elevator_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 from decouple import config
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('MODE') == 'dev' else False
